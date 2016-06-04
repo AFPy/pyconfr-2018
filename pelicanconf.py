d66ad6f52c491366a2d1db0ee8e0fb6fe4c064e1
Guillaume Ayoub <guillaume.ayoub@kozea.fr>
2016-06-04 19:03:32 +0200

    Don't use pure anymore

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,33 +20,35 @@ SOCIAL = (('Github', 'https://github.com/AFPy/pyconfr_2016'),)
 
 DEFAULT_PAGINATION = False
 
-THEME = "pure"
+THEME = "theme"
 INDEX_SAVE_AS = 'nouvelles.html'
 
 COVER_IMG_URL = '/theme/sidebar.jpg'
 
 SOCIAL = (
-    ('envelope', 'mailto:contact@pycon.fr'),
-    ('rss', SITEURL + '/feeds/all.atom.xml'),
-    ('github', 'https://github.com/AFPy/pyconfr_2016'),
+    ('Email', 'mailto:contact@pycon.fr'),
+    ('RSS', SITEURL + '/feeds/all.atom.xml'),
+    ('GitHub', 'https://github.com/AFPy/pyconfr_2016'),
 )
 
 MENUITEMS = (
-    (u'À propos', 'index.html'),
-    (u'Nouvelles', 'archives.html'),
-    (u'Programme', 'pages/programme.html'),
-    (u'Venir', 'pages/venir.html'),
-    (u'Se loger', 'pages/se-loger.html'),
-    (u'Miam !', 'pages/se-restaurer.html'),
-    (u'Soutiens', 'pages/soutiens.html'),
-    (u'Nous contacter', 'pages/nous-contacter.html'),
-    (u'Code de conduite', 'pages/code-de-conduite.html'),
+    (u'À propos', '/index.html'),
+    (u'Nouvelles', '/archives.html'),
+    (u'Programme', '/pages/programme.html'),
+    (u'Venir', '/pages/venir.html'),
+    (u'Se loger', '/pages/se-loger.html'),
+    (u'Miam !', '/pages/se-restaurer.html'),
+    (u'Soutiens', '/pages/soutiens.html'),
+    (u'Nous contacter', '/pages/nous-contacter.html'),
+    (u'Code de conduite', '/pages/code-de-conduite.html'),
 )
+DISPLAY_PAGES_ON_MENU = False
+DISPLAY_CATEGORIES_ON_MENU = False
 STATIC_PATHS = ['images', 'documents', 'extra/CNAME', ]
 EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['post_stats', 'html_rst_directive']
+PLUGINS = ['post_stats', 'html_rst_directive', 'assets']
 RESPONSIVE_IMAGES = True
 
 RELATIVE_URLS = True
